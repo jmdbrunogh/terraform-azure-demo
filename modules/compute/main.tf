a15be702f53b1f455643ccc5919ae0f2abfdc3be
João Marcelo Domingos Bruno <104376632+jmdbrunogh@users.noreply.github.com>
2025-05-16 12:19:41 -0300

    feat: adiciona módulo compute com VM no Azure

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,1 +1,38 @@
+resource "azurerm_network_interface" "nic" {
+  name                = "vm-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                = "demo-vm"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  admin_password      = "P@ssw0rd1234!" # Para demo. Em produção, use Secrets!
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  os_disk {
+    name              = "demo-osdisk"
+    caching           = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
 
--- modules/compute/outputs.tf
+++ modules/compute/outputs.tf
@@ -1,1 +1,8 @@
+output "vm_id" {
+  value = azurerm_windows_virtual_machine.vm.id
+}
+
+output "vm_private_ip" {
+  value = azurerm_network_interface.nic.private_ip_address
+}
 
